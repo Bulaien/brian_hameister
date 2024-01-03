899e52f37fa7ad44919fea116ae5e44fe16d2878
brian <brian>
2024-01-03 20:05:53 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navi from './Navi'
+import Products from './Products'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="main">
+
+<Navi />
+    <Products />
+    </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Navi.jsx
@@ -0,0 +1,13 @@
+function Navi(){
+return(
+    
+   <div className="navi">
+    <div className="zentrum">
+    <h1>Bulaien-IT</h1>
+    </div>
+   </div>
+)
+
+}
+
+export default Navi
